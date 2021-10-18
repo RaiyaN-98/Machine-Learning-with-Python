8ce118bd8d200d82d8d7eab06af8e04bf61ff6e7
RaiyaN-98 <raiyan1698@gmail.com>
2021-10-18 22:34:23 +0600

    final

--- svm.py
+++ svm.py
@@ -6,3 +6,84 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
+cell_df = pd.read_csv("cell_samples.csv")
+
+## Data vizualization
+ax = cell_df[cell_df['Class'] == 4][0:50].plot(kind='scatter', x='Clump', y='UnifSize', color='DarkBlue', label='malignant')
+cell_df[cell_df['Class'] == 2][0:50].plot(kind='scatter', x='Clump', y='UnifSize', color='Yellow', label='benign', ax=ax)
+
+## Data preprocessing
+print(cell_df.dtypes)
+# printing the data types it can be seen that some of 'BareNuc' column is not numeric
+# we need to remove those
+cell_df = cell_df[pd.to_numeric(cell_df['BareNuc'], errors='coerce').notnull()]
+cell_df['BareNuc'] = cell_df['BareNuc'].astype('int')
+print(cell_df.dtypes) 
+
+## Extracting features
+feature_df = cell_df[['Clump', 'UnifSize', 'UnifShape', 'MargAdh', 'SingEpiSize', 'BareNuc', 'BlandChrom', 'NormNucl', 'Mit']]
+X = np.asarray(feature_df)
+
+cell_df['Class'] = cell_df['Class'].astype('int')
+y = np.asarray(cell_df['Class'])
+
+## Train/Test set separation
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
+
+## SVM model
+from sklearn import svm
+clf = svm.SVC(kernel = 'rbf')
+clf.fit(X_train, y_train)
+
+yhat = clf.predict(X_test)
+
+## Evaluation
+from sklearn.metrics import classification_report, confusion_matrix
+
+import itertools
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# compute confussion matix
+cnf_matrix = confusion_matrix(y_test, yhat, labels=[2,4])
+np.set_printoptions(precision=2)
+
+print(classification_report(y_test, yhat))
+
+# plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=['Benign(2)', 'Malignant'])
+
+# f1_score directly
+from sklearn.metrics import f1_score
+print(f1_score(y_test, yhat, average='weighted'))
+
+# jaccard index
+from sklearn.metrics import jaccard_score
+print(jaccard_score(y_test, yhat, pos_label=2))
+
+plt.show()
